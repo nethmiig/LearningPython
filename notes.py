1e0602a0bd7340aedd4c997b28ca67e627a888da
Nethmi Gamage <enega002@edu.xamk.fi>
2023-09-13 12:14:49 +0300

    argv

--- notes.py
+++ notes.py
@@ -5,9 +5,19 @@ import sys
 
 #main function
 def main() -> int:
+    #Arguements 
+    print(sys.argv)
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2])
+
+    print(sys.argv[1]+sys.argv[2])
+    print(23+32)
+    a=int(sys.argv[1])
+    b=int(sys.argv[2])
+    print(a+b)
+#return value
     return(0)
-
 #main function definition
-
 if __name__ == '__main__':
     sys.exit(main())
