c7bc9b5328725a39f18a5eaf04b6e2ae7f73840e
Nethmi Gamage <enega002@edu.xamk.fi>
2023-09-18 14:24:28 +0300

    New branch

--- Note.py
+++ Note.py
@@ -2,4 +2,14 @@ import sys
 
 def main() -> int:
     print("ADD/RETRIEVE/DELETE")
-    userinput = 
+    print("Enter choice: ")  
+    userinput = input()
+    if userinput== "add":
+        print("Enter your note here: ")
+        addnote=input()
+        addnote= print(sys.argv)
+        print(sys.argv[1]+ sys.argv[2])
+
+    return(0)
+if __name__ == '__main__':
+    sys.exit(main())
