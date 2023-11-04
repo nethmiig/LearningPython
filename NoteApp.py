cfd4296cb4178b93d08db17e043ac1332f248620
Nethmi Gamage <enega002@edu.xamk.fi>
2023-11-04 15:01:27 +0200

    Defined Retrieve note function

--- NoteApp.py
+++ NoteApp.py
@@ -10,13 +10,20 @@ notes = {}  # Dictionary to store notes for each user
 # Function to create a note
 def create_note(username):
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+    subject = input("Enter the subject: ")
+    note_text = input("Enter the note text: ")
+    note = {"date": date, "subject": subject, "note_text": note_text}
     if username in notes:
         notes[username].append(note)
     else:
         notes[username] = [note]
     print("Note created successfully!")
 
+
+# Function to retrieve and display notes
+
+
+def retrieve_notes(username):
     # Main application loop
     while True:
         username = input("Enter your username: ")
