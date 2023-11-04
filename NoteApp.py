40ac5e27b8222642761b73aef2acd5de824b7062
Nethmi Gamage <enega002@edu.xamk.fi>
2023-11-04 14:37:07 +0200

    Defined a function to create notes

--- NoteApp.py
+++ NoteApp.py
@@ -3,18 +3,21 @@
 users = {"user1": "password1", "user2": "password2"}
 notes = {}  # Dictionary to store notes for each user
 
-while True:
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
 
-    if username in users and users[username] == password:
-        print(f"Welcome, {username}!")
-        while True:
-            print("\nOptions:")
-            print("1. Create Note")
-            print("2. Retrieve Notes")
-            print("3. Delete Notes")
-            print("4. Logout")
-            choice = input("Enter your choice: ")
-else:
-    print("Invalid credentials. Please try again.")
+# Function to create a note
+def create_note(username):
+    while True:
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if username in users and users[username] == password:
+            print(f"Welcome, {username}!")
+            while True:
+                print("\nOptions:")
+                print("1. Create Note")
+                print("2. Retrieve or Read Notes")
+                print("3. Delete Notes")
+                print("4. Logout")
+                choice = input("Enter your choice: ")
+    else:
+        print("Invalid credentials. Please try again.")
